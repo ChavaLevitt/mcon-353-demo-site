ce21cbc4a10ef84c267e26e93624138518c86796
c_levitt <clevitt2@student.touro.edu>
2022-02-26 21:09:16 -0500

    added header to switch between pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,12 +3,20 @@ import React, { useState } from "react";
 
 import { Home } from "../home/home";
 import { ToDo } from "../todo/todo";
+import { Header } from "../header/header";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      {/*<Home />*/}
-      <ToDo />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/todo" element={<ToDo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
